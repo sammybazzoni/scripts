0f928d4c45e0ac8cfd24759dbb94245ab7ecb230
sammybazzoni <60040142+sammybazzoni@users.noreply.github.com>
2026-05-10 12:32:39 -0500

    Update firmware-util.sh

--- firmware-util.sh
+++ firmware-util.sh
@@ -17,7 +17,7 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 #where the stuff is
-script_url="https://raw.githubusercontent.com/sammybazzoni/scripts/main/"
+script_url="https://raw.githubusercontent.com/sammybazzoni/scripts/test/"
 
 #ensure output of system tools in en-us for parsing
 export LC_ALL=C
